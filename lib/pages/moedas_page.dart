ad219a4ef6e30b661394a33d5e522219c2b1174a
Cleiton Junior <cleiton.junior@redeinova.com>
2023-11-07 16:21:27 -0300

    listagem de moedas

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,3 +1,4 @@
+import 'package:cripto_moedas/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/widget_drawer_custom.dart';
@@ -7,6 +8,8 @@ class MoedasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tabela = MoedaRepository.tabela;
+
     return Scaffold(
       drawer: const Drawer(
         clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -22,7 +25,20 @@ class MoedasPage extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            leading: Image.asset(tabela[moeda].icone),
+            title: Text(tabela[moeda].nome),
+            trailing: Text(
+              tabela[moeda].preco.toString(),
+            ),
+          );
+        },
+        padding: const EdgeInsets.all(16),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: tabela.length,
+      ),
     );
   }
 }
